fad975babe160da9b9fad85624893dc038253fb7
whoisyuma <whoisyuma.0913@gmail.com>
2025-09-18 12:25:33 +0900

    add link

--- app/recipes/layout.tsx
+++ app/recipes/layout.tsx
@@ -11,7 +11,7 @@ export default function RecipesLayout({children}: {children: React.ReactNode;}) 
     return (
         <div>
             <header className="flex justify-between items-center px-6 py-4 bg-amber-300 shadow">
-                <h1 className="text-xl font-bold text-gray-800 py-1.5">マイレシピ帳</h1>
+                <Link href='/recipes' className="text-xl font-bold text-gray-800 py-1.5">マイレシピ帳</Link>
                 {showHeader && (
                     <Link href="/recipes/create" className="bg-orange-400 text-white px-4 py-2 rounded-md font-medium shadow transition duration-200 hover:bg-orange-500 hover:scale-105 focus:ring-2 focus:ring-orange-400 focus:ring-offset-2">＋レシピを追加</Link>
                 )}
